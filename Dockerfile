d62aa0e85d471b1bb9651916d9969e297cebe01d
Rohan Padmanabhan <rohan@padmanabhan.net>
2019-01-26 19:26:02 +0000

    Add travis yml, deploy script and Dockerfile.

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM golang:1.11.2-alpine3.8
+
+WORKDIR /go/src/github.com/kine-dmd/api/
+
+COPY . .
+
+RUN apk add --no-cache git
+RUN apk add --no-cache gcc
+RUN apk add --no-cache libc-dev
+
+RUN wget -O dep https://github.com/golang/dep/releases/download/v0.5.0/dep-linux-amd64
+RUN echo '287b08291e14f1fae8ba44374b26a2b12eb941af3497ed0ca649253e21ba2f83  dep' | sha256sum -c -
+RUN chmod +x dep
+RUN mv dep /usr/bin/
+
+
+RUN dep ensure
+
+RUN go test -v ./...
+
+RUN rm -f **/*_test.go
+
+RUN go build -o ~/go/bin/main .
+
+ENTRYPOINT ~/go/bin/main
+
+EXPOSE 80
---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+CLUSTER_NAME=kine-dmd
+IMAGE_REPO_URL=736634562271.dkr.ecr.eu-west-2.amazonaws.com/kine-dmd-api
+
+pip install --user awscli
+export PATH=$PATH:$HOME/.local/bin 
+
+# install ecs-deploy
+add-apt-repository ppa:eugenesan/ppa
+apt-get update
+apt-get install jq -y
+curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | sudo tee -a /usr/bin/ecs-deploy
+sudo chmod +x /usr/bin/ecs-deploy
+
+if [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Deploying services to production"
+  docker --version
+  $(aws ecr get-login --no-include-email --region eu-central-1) #needs AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY envvars
+
+  docker build -t kine-dmd-api -f kine-dmd/Dockerfile kine-dmd
+  docker tag kine-dmd:latest 736634562271.dkr.ecr.eu-west-2.amazonaws.com/kine-dmd-api:latest
+  docker push 736634562271.dkr.ecr.eu-west-2.amazonaws.com/kine-dmd-api:latest
+  ecs-deploy -c $CLUSTER_NAME -n kine-dmd-api -i $IMAGE_REPO_URL:latest
+
+  echo "Service deployed"
+else 
+  echo "No deployment necessary"
+fi
