64d65ea60520cfab6c307fc3baf62475462ece47
Rohan Padmanabhan <rohan@padmanabhan.net>
2019-03-05 23:37:46 +0000

    Check uuid is valid.

--- apple-watch-3/apple-watch-3.go
+++ apple-watch-3/apple-watch-3.go
@@ -3,6 +3,7 @@ package apple_watch_3
 import (
 	"github.com/gorilla/mux"
 	"github.com/kine-dmd/api/kinesisqueue"
+	"github.com/satori/go.uuid"
 	"io/ioutil"
 	"log"
 	"net/http"
@@ -35,28 +36,43 @@ func Init(r *mux.Router) {
 func binaryHandler(writer http.ResponseWriter, request *http.Request) {
 	// Extract the UUID from the URL
 	vars := mux.Vars(request)
-	uuid := vars["uuid"]
+	watchId := vars["uuid"]
+
+	// Check the uuid is valid
+	if !isValidUUID(watchId) {
+		http.Error(writer, "Bad UUID.", http.StatusBadRequest)
+		return
+	}
 
 	// Check there is a body
 	if request.ContentLength == 0 {
-		http.Error(writer, "0 length body.", http.StatusExpectationFailed)
+		http.Error(writer, "0 length body.", http.StatusBadRequest)
 		return
 	}
 
 	// Entire body is data so read
 	data, err := ioutil.ReadAll(request.Body)
 	if err != nil {
-		log.Println("Unable to read body from Apple Watch 3. UUID: ", uuid)
+		log.Println("Unable to read body from Apple Watch 3. UUID: ", watchId)
 		http.Error(writer, "Unable to read POST body data", http.StatusUnprocessableEntity)
 		return
 	}
 
-	// Package the binary data together along with the uuid
-	structuredData := unparsedAppleWatch3Data{WatchPosition: watchPosition{uuid, 1}, RawData: data}
+	// Package the binary data together along with the watchId
+	structuredData := unparsedAppleWatch3Data{WatchPosition: watchPosition{watchId, 1}, RawData: data}
 
 	// Send it to the relevant kinesis queue
-	err = queue.SendToQueue(structuredData, uuid)
+	err = queue.SendToQueue(structuredData, watchId)
 	if err != nil {
 		http.Error(writer, "Server unable to forward data", http.StatusInternalServerError)
 	}
+}
+
+func isValidUUID(uid string) bool {
+	// Try and parse the uuid to a string
+	_, err := uuid.FromString(uid)
+	if err != nil {
+		return false
+	}
+	return true
 }
