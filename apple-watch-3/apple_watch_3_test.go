61693e4bbcc20e62c0cadc33539eb8ac49719d55
Rohan Padmanabhan <rohan@padmanabhan.net>
2019-03-08 17:44:35 +0000

    Use new body for each test.

--- apple-watch-3/apple_watch_3_test.go
+++ apple-watch-3/apple_watch_3_test.go
@@ -3,43 +3,59 @@ package apple_watch_3
 import (
 	"bytes"
 	"github.com/gorilla/mux"
-	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 // A router to use when testing
-var router *mux.Router = mux.NewRouter()
-var body io.Reader = bytes.NewReader([]byte{0, 0, 0})
+var router *mux.Router
 
 // Run before each test
 func init() {
+	print("Init run")
+	router = mux.NewRouter()
 	Init(router)
 }
 
 func TestWrongLengthUUID(t *testing.T) {
+	body := bytes.NewReader([]byte{1, 2, 3})
 	req, _ := http.NewRequest("POST", "/upload/apple-watch-3/someuuid", body)
 	response := sendRequest(req)
 	checkResponseCode(t, http.StatusBadRequest, response.Code)
 }
 
 func TestUUIDNotBase64(t *testing.T) {
+	body := bytes.NewReader([]byte{1, 2, 3})
 	req, _ := http.NewRequest("POST", "/upload/apple-watch-3/ZZZZZZZZ-XXXX-YYYY-UUUU-WWWWWWWWWWWW", body)
 	response := sendRequest(req)
 	checkResponseCode(t, http.StatusBadRequest, response.Code)
 }
 
 func TestValidUUID(t *testing.T) {
+	body := bytes.NewReader([]byte{1, 2, 3})
 	req, _ := http.NewRequest("POST", "/upload/apple-watch-3/00000000-0000-0000-0000-000000000000", body)
 	response := sendRequest(req)
 	checkResponseCode(t, http.StatusOK, response.Code)
 }
 
 func TestValidUUIDWithoutDashes(t *testing.T) {
-	req, _ := http.NewRequest("POST", "/upload/apple-watch-3/00000000000000000000000000000000", body)
+	body := bytes.NewReader([]byte{1, 2, 3})
+	req, _ := http.NewRequest("POST", "/upload/apple-watch-3/00000000000000000000000000000001", body)
 	response := sendRequest(req)
 	checkResponseCode(t, http.StatusOK, response.Code)
+}
+
+func TestNilBody(t *testing.T) {
+	req, _ := http.NewRequest("POST", "/upload/apple-watch-3/00000000-0000-0000-0000-000000000000", nil)
+	response := sendRequest(req)
+	checkResponseCode(t, http.StatusBadRequest, response.Code)
+}
+
+func Test0LenBody(t *testing.T) {
+	req, _ := http.NewRequest("POST", "/upload/apple-watch-3/00000000-0000-0000-0000-000000000000", bytes.NewReader([]byte{}))
+	response := sendRequest(req)
+	checkResponseCode(t, http.StatusBadRequest, response.Code)
 }
 
 func sendRequest(req *http.Request) *httptest.ResponseRecorder {
