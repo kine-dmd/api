e02269739140bb74d217ec0985ed003119b6dea5
Rohan Padmanabhan <rohan@padmanabhan.net>
2019-04-12 23:56:10 +0100

    Add test for CSV watch position database.

---
+++ watch_position_db/csv_wp_db_test.go
@@ -0,0 +1,41 @@
+package watch_position_db
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+	"testing"
+)
+
+func makeCSVFile() {
+	// Make a new file
+	file, _ := os.Create("watch_positions.csv")
+	defer file.Close()
+
+	// Write the data to the file
+	origUUIDs, origPatientIds, origLimbs := makeFakePositionData()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	for i := range origUUIDs {
+		_ = writer.Write([]string{origUUIDs[i],
+			origPatientIds[i],
+			strconv.FormatFloat(origLimbs[i], 'f', 0, 64)})
+	}
+}
+
+func removeCSVFile() {
+	_ = os.Remove("watch_positions.csv")
+}
+
+func TestScanCSVDatabase(t *testing.T) {
+	// Make a CSV file for testing and delete it after test completion
+	makeCSVFile()
+	defer removeCSVFile()
+
+	// Make the table and take a scan of it
+	csvDB := MakeCSVWatchPositionDB()
+	scan := csvDB.GetTableScan()
+
+	// Use the standard compare results function
+	compareResults(scan, t)
+}
