bed3afb0b3c2b78d0a402b2b75a417ca8b03423b
Rohan Padmanabhan <rohan@padmanabhan.net>
2019-05-02 18:25:08 +0100

    Split the kinesis data if it is too large to be written in one go.

--- apple_watch_3/kinesis_data_writer_test.go
+++ apple_watch_3/kinesis_data_writer_test.go
@@ -20,6 +20,34 @@ func TestSendingToQueue(t *testing.T) {
 	mockCtrl.Finish()
 }
 
+func TestSplittingLargeItems(t *testing.T) {
+	// Create mocks and fake data
+	mockCtrl, mockQueue := makeMockQueue(t)
+	watchData := makeFakeUnparsedDataStruct("dmd01", 1, make([]byte, 1000000))
+	mockQueue.EXPECT().SendToQueue(gomock.Any(), watchData.WatchPosition.PatientID).Return(nil).Times(2)
+
+	// Make a kinesis data writer and send to it
+	kinesisDataWriter := MakeKinesisDataWriter(mockQueue)
+	_ = kinesisDataWriter.writeData(watchData)
+
+	// Check expectations have been satisfied
+	mockCtrl.Finish()
+}
+
+func TestTripleSplit(t *testing.T) {
+	// Create mocks and fake data
+	mockCtrl, mockQueue := makeMockQueue(t)
+	watchData := makeFakeUnparsedDataStruct("dmd01", 1, make([]byte, 1800000))
+	mockQueue.EXPECT().SendToQueue(gomock.Any(), watchData.WatchPosition.PatientID).Return(nil).Times(3)
+
+	// Make a kinesis data writer and send to it
+	kinesisDataWriter := MakeKinesisDataWriter(mockQueue)
+	_ = kinesisDataWriter.writeData(watchData)
+
+	// Check expectations have been satisfied
+	mockCtrl.Finish()
+}
+
 func makeMockQueue(t *testing.T) (*gomock.Controller, *kinesisqueue.MockKinesisQueueInterface) {
 	// Make a mock for the kinesis queue
 	mockCtrl := gomock.NewController(t)
